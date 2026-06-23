6d6bb6536ff4939f47af68f1a2c9b793b9539a4c
hieudz2504 <hieudz2504@gmail.com>
2026-06-23 22:02:36 +0700

    Du lieu view+du lieu view

--- DALTUDTXD_68TH4_PMTKVK_REVITAPI/DALTUDTXD_68TH4_PMTKVK_REVITAPI/Views/DuLieuView.xaml.cs
+++ DALTUDTXD_68TH4_PMTKVK_REVITAPI/DALTUDTXD_68TH4_PMTKVK_REVITAPI/Views/DuLieuView.xaml.cs
@@ -2,12 +2,14 @@ using System.Windows.Controls;
 
 namespace DALTUDTXD_68TH4_PMTKVK_REVITAPI.Views
 {
-    public partial class DuLieuView.xaml : UserControl
+    /// <summary>
+    /// Interaction logic for DuLieuView.xaml
+    /// </summary>
+    public partial class DuLieuView : UserControl
     {
-        public DuLieuView.xaml()
+        public DuLieuView()
         {
             InitializeComponent();
         }
     }
 }
-
